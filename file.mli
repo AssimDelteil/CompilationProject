4932dc5c0fc3d1c38859386a657074540360968f
Delta <assim.delteil@ensiie.fr>
2021-12-29 17:41:45 +0100

    first commit, q1: first definitions

---
+++ file.mli
@@ -0,0 +1,37 @@
+type file =
+
+type declaration = 
+
+type instruction = 
+
+type expr = 
+    |Plus of expr*expr
+    |Moins of expr*expr
+    |Fois of expr*expr
+    |Div of expr*expr
+    |Puiss of expr*expr
+    |Eq of expr*expr
+    |Neq of expr*expr
+    |LessE of expr*expr
+    |LessT of expr*expr
+    |GreatE of expr*expr
+    |GreatT of expr*expr
+    |Mod of expr*expr
+    |Rem of expr*expr
+    |And of expr*expr
+    |Or of expr*expr
+    |Xor of expr*expr
+
+    |Id of string 
+    |Cst of float (*Pas sur que ça soit float, implémentation compliquée *)
+
+    |Nega of expr
+    |Abs of expr
+    |Not of expr 
+
+    |AndThen of expr*expr
+    |OrElse of expr*expr
+
+    |ConvOuAppelFct of string*(expr list) 
+    |Paren of expr
+ 
